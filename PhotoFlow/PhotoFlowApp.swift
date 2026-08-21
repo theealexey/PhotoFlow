2b9519024eac42b44b8975b1fc8d14611aa80214
theealexey <theealexey@gmail.com>
2026-08-21 22:02:54 +0300

    Initial Commit

---
+++ PhotoFlow/PhotoFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoFlowApp.swift
+//  PhotoFlow
+//
+//  Created by Alexey Westergaard on 21.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct PhotoFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
